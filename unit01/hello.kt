e7a71adc575932c0dc6371652876581a0147c6a4
Francisco I. Rodriguez <firodriguez@dcccd.edu>
2023-02-27 21:35:17 -0600

    Add files via upload

---
+++ unit01/hello.kt
@@ -0,0 +1,13 @@
+package unit01
+
+// This is my first Kotlin program.
+fun main() {
+    println("Hello World!")
+    println("I am learning Kotlin.")
+    println("It is awesome!")
+}
+
+// Pseudocode:
+// The program defines a function called "unit02.main" that takes an array of strings as input.
+// The function prints the string "Hello World!" (and others) to the console.
+// This is a simple program that outputs a greeting message to the user.
