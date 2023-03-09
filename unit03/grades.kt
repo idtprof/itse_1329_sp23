c4a10ff6e2d242d765142e7e6308f6680035a248
Francisco I. Rodriguez <firodriguez@dcccd.edu>
2023-03-09 10:33:48 -0600

    updated grades

--- unit03/grades.kt
+++ unit03/grades.kt
@@ -1,22 +1,32 @@
-// Esperanza Macias
-// Feb 25 2023
-
 fun main() {
-    // Get the person's weight in pounds and height in inches
-    print("Enter weight in pounds: ")
-    val weight = readLine()!!.toDouble()
+    val sales = mutableListOf<Int>()
+    val stores = listOf("Store 1", "Store 2", "Store 3", "Store 4", "Store 5")
 
-    print("Enter height in inches: ")
-    val height = readLine()!!.toDouble()
+    // ask user to enter today's sales for each store
 
-    // Calculate the person's BMI
-    val bmi = weight * 703 / (height * height)
+    for (store in stores)
+    {
+        print("Enter today's sales for $store: ")
+        val sale = readLine()?.toIntOrNull()
+        if (sale != null) {
+            sales.add(sale)
+        }
+        else {
+            println("Invalid input, try again.")
+            break
+        }
+    }
 
-    // Display the BMI and weight status
-    println("BMI: %.1f".format(bmi))
-    when {
-        bmi < 18.5 -> println("Underweight")
-        bmi <= 25 -> println("Optimal weight")
-        else -> println("Overweight")
+    // display bar chart comparing each store sales
+
+    for (i in 0 until stores.size) {
+        print("${stores[i]}: ")
+        val salesInDollars = sales[i] / 100
+        for (j in 0 until salesInDollars)
+        {
+            print("*")
+        }
+
+        println()
     }
 }
