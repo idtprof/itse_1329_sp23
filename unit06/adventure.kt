5132f3ce7297a90b6195b20bef8a4fffebc5913a
Francisco Rodriguez <firodriguez@dcccd.edu>
2023-04-11 08:13:08 -0500

    working adventure

--- unit06/adventure.kt
+++ unit06/adventure.kt
@@ -1,51 +1,59 @@
-package unit06
-
 fun main() {
-    val rooms = arrayOf(
-        arrayOf("You are in a dusty castle room.\nPassages lead to the north and south.", "1", "-1", "-1", "2"),
-        arrayOf("You are in the armory.\nThere is a room off to the south.", "-1", "0", "-1", "-1"),
-        arrayOf("You are in a torch-lit hallway.\nThere are rooms to the east and west.", "-1", "-1", "0", "3"),
-        arrayOf("You are in a bedroom. A window overlooks the castle courtyard.\nA hallway is to the west.", "-1", "2", "-1", "-1"),
-        arrayOf("You are in the kitchen. It looks like a roast is being made for supper.\nA hallway is to the east.", "-1", "-1", "-1", "2")
-    )
+    // Create an array to store room information
+    val roomList = arrayOfNulls<Array<Any?>>(7)
 
+    // Room 0 -- Bedroom 2
+    roomList[0] = arrayOf("You are in a bedroom with a bed and a closet. There are exits to the North and to the East.", 3, 1, null, null)
+
+    // Room 1 -- South Hall
+    roomList[1] = arrayOf("You are in the South Hall with paintings on the walls. There are exits to the North, East, and West.", 4, 2, null, 0)
+
+    // Room 2 -- The Dining Room
+    roomList[2] = arrayOf("You are in a dark, cold dining room with a large table. There are exits to the East and North.", 5, null, null, 1)
+
+    // Room 3 -- Bedroom 1
+    roomList[3] = arrayOf("You enter another bedroom with an old dusty bed and a window. There are exits to the East and South.", null, 4, 0, null)
+
+    // Room 4 -- North Hall
+    roomList[4] = arrayOf("You're walking down the North Hall with portraits of people you don't recognize. There are exits in all four directions.", 6, 5, 1, 3)
+
+    // Room 5 -- The Kitchen
+    roomList[5] = arrayOf("You are in a kitchen with a stove, an old refrigerator, and ripped grocery bags. There are exits to the West and South.", null, null, 2, 4)
+
+    // Room 6 -- The Balcony
+    roomList[6] = arrayOf("You step onto a balcony overlooking a garden. There are exits to the South.", null, null, 4, null)
+
+    // Initialize the current room and the done flag
     var currentRoom = 0
+    var done = false
 
-    while (true) {
-        println(rooms[currentRoom][0])
-        print("What direction? ")
-        val direction = readLine()!!.trim().toLowerCase()
+    // Main game loop
+    while (!done) {
+        // Display the current room description
+        println()
+        println(roomList[currentRoom]?.get(0))
 
-        when (direction) {
-            "n" -> {
-                if (rooms[currentRoom][1] != "-1") {
-                    currentRoom = rooms[currentRoom][1].toInt()
-                } else {
-                    println("Can't go that way.")
-                }
+        // Prompt the user for the next move
+        print("What do you want to do? (n: North, e: East, s: South, w: West) ")
+        val cmd = readLine()?.lowercase()
+
+        // Determine the next room based on the user's input
+        val nextRoom: Int? = when (cmd) {
+            "n" -> roomList[currentRoom]?.get(1) as Int?
+            "e" -> roomList[currentRoom]?.get(2) as Int?
+            "s" -> roomList[currentRoom]?.get(3) as Int?
+            "w" -> roomList[currentRoom]?.get(4) as Int?
+            else -> {
+                println(">>>>> Invalid input. Please enter n, e, s, or w.")
+                null
             }
-            "e" -> {
-                if (rooms[currentRoom][2] != "-1") {
-                    currentRoom = rooms[currentRoom][2].toInt()
-                } else {
-                    println("Can't go that way.")
-                }
-            }
-            "s" -> {
-                if (rooms[currentRoom][3] != "-1") {
-                    currentRoom = rooms[currentRoom][3].toInt()
-                } else {
-                    println("Can't go that way.")
-                }
-            }
-            "w" -> {
-                if (rooms[currentRoom][4] != "-1") {
-                    currentRoom = rooms[currentRoom][4].toInt()
-                } else {
-                    println("Can't go that way.")
-                }
-            }
-            else -> println("Invalid direction. Please enter 'n', 'e', 's', or 'w'.")
+        }
+
+        // Check if the chosen direction is valid and update the current room or display an error message
+        if (nextRoom == null) {
+            println(">>>>> You cannot go that way.")
+        } else {
+            currentRoom = nextRoom
         }
     }
 }
